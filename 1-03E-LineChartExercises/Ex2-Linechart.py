c4e37737f64fff39ef561c6faa6bb306575641af
Trevor James Smith <trevor_smith@live.com>
2019-01-15 16:36:07 -0500

    ex2-linecharts solved

--- 1-03E-LineChartExercises/Ex2-Linechart.py
+++ 1-03E-LineChartExercises/Ex2-Linechart.py
@@ -5,27 +5,29 @@
 ######
 
 # Perform imports here:
-
-
-
+import plotly.graph_objs as go
+import plotly.offline as pyo
+import pandas as pd
 
 # Create a pandas DataFrame from 2010YumaAZ.csv
-df = pd.read_csv('../data/2010YumaAZ.csv')
-days = ['TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY','MONDAY']
-
+df = pd.read_csv('../Data/2010YumaAZ.csv')
+days = ['TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY', 'MONDAY']
 
 # Use a for loop (or list comprehension to create traces for the data list)
 data = []
 
 for day in days:
     # What should go inside this Scatter call?
-    trace = go.Scatter()
+    trace = go.Scatter(
+        x=df['LST_TIME'],
+        y=df[df['DAY'] == day]['T_HR_AVG'],
+        mode='lines',
+        name=day)
     data.append(trace)
 
 # Define the layout
-
-
-
-
+layout = go.Layout(title='Daily temp averages')
 
 # Create a fig from data and layout, and plot the fig
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig)
