20a69049d2cffa03e11173594816b2ca77f664ed
Zeitsperre <trevor_smith@live.com>
2019-04-08 16:49:33 -0400

    Answers

--- 1-08E-DistplotExercises/Ex7-Distplot.py
+++ 1-08E-DistplotExercises/Ex7-Distplot.py
@@ -1,30 +1,33 @@
 #######
 # Objective: Using the iris dataset, develop a Distplot
 # that compares the petal lengths of each class.
-# File: '../data/iris.csv'
+# File: '../Data/iris.csv'
 # Fields: 'sepal_length','sepal_width','petal_length','petal_width','class'
 # Classes: 'Iris-setosa','Iris-versicolor','Iris-virginica'
 ######
 
 # Perform imports here:
-
-
-
+import plotly.figure_factory as ff
+import plotly.offline as pyo
+import numpy as np
+import pandas as pd
 
 # create a DataFrame from the .csv file:
-
-
+df = pd.read_csv('../Data/iris.csv')
 
 # Define the traces
+setosa = df[df['class']=='Iris-setosa']['petal_length']
+versicolor = df[df['class']=='Iris-versicolor']['petal_length']
+virginica = df[df['class']=='Iris-virginica']['petal_length']
 
 # HINT:
 # This grabs the petal_length column for a particular flower
-df[df['class']=='Iris-some-flower-class']['petal_length']
-
-
+# df[df['class']=='Iris-some-flower-class']['petal_length']
 
 # Define a data variable
-
-
+data = [setosa, versicolor, virginica]
+layout = ['setosa', 'versicolor', 'virginica']
 
 # Create a fig from data and layout, and plot the fig
+fig = ff.create_distplot(data, layout)
+pyo.plot(fig)
--- 1-09E-HeatmapExercises/Ex8-Heatmap.py
+++ 1-09E-HeatmapExercises/Ex8-Heatmap.py
@@ -8,21 +8,21 @@
 ######
 
 # Perform imports here:
-
+import plotly.graph_objs as go
+import plotly.offline as pyo
+import pandas as pd
 
 
 # Create a DataFrame from  "flights" data
-df = pd.read_csv('../data/flights.csv')
+df = pd.read_csv('../Data/flights.csv')
 
 # Define a data variable
-
-
-
-
-
+trace = go.Heatmap(x=df['year'], y=df['month'], z=df['passengers'], colorscale='Greens')
+data = [trace]
 
 # Define the layout
-
-
+layout = go.Layout(title='Flights')
 
 # Create a fig from data and layout, and plot the fig
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig)
